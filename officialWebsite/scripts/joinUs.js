bdd876bda3a9b16fa9ebd5bee39d2ae5f39c087a
xinyu.cai <xinyu.cai@suanhua.org>
2017-04-05 20:54:47 +0800

    pages

--- officialWebsite/scripts/joinUs.js
+++ officialWebsite/scripts/joinUs.js
@@ -1,47 +1,68 @@
 $(function(){
-    function map_mouseover(className) {
-        switch (className) {
-            case "sh":
-                $("#box_header").text("shanghai");
-                $("#box_littleheader").text("上海");
-                $("#box_address").text("地址：上海市闵行区吴中路699号 全季酒店1F-2F");
-                $("#box_tel").text("电话：400-100-1999");
-                $("#box_fax").text("传真：021-29989981");
-                break;
-            case "bj":
-                $("#box_header").text("beijing");
-                $("#box_littleheader").text("北京");
-                $("#box_address").text("地址：北京 朝阳区 大山子彩虹路6号 全季酒店1F");
-                $("#box_tel").text("电话：400-100-1999");
-                $("#box_fax").text("传真：021-29989981");
-                break;
-            case "wh":
-                $("#box_header").text("wuhan");
-                $("#box_littleheader").text("武汉");
-                $("#box_address").text("地址：武汉市东湖高新技术开发区关山大道289号禧玥酒店4F");
-                $("#box_tel").text("电话：400-100-1999");
-                $("#box_fax").text("传真：021-29989981");
-                break;
-        }
-    };
 
-    $(".map_inner .sh").mouseover(function() {
-        map_mouseover("sh");
-        $(this).addClass("sh_hover");
-        $(".map_add .bj").removeClass("bj_hover");
-        $(".map_add .wh").removeClass("wh_hover");
+    $(".more .txt").hover(function(){
+        $(this).parents(".timeLine-date").addClass("active");
+    },function(){
+        $(this).parents(".timeLine-date").removeClass("active");
     });
 
-    $(".map_inner .bj").mouseover(function() {
-        map_mouseover("bj");
-        $(this).addClass("bj_hover");
-        $(".map_add .sh").removeClass("sh_hover");
-        $(".map_add .wh").removeClass("wh_hover");
+
+    $(".job").on('click', function() {
+        $(this).addClass("hover").siblings().removeClass("hover");
     });
-    $(".map_inner .wh").mouseover(function() {
-        map_mouseover("wh");
-        $(this).addClass("wh_hover");
-        $(".map_add .sh").removeClass("sh_hover");
-        $(".map_add .bj").removeClass("bj_hover");
+
+
+
+
+    //function mapClick(className) {
+    //    switch (className) {
+    //        case "sh":
+    //            $(".sh-show").addClass("active");
+    //            $(".sz-show,.bj-show").removeClass("active");
+    //            $(".sz,.bj").removeClass("active");
+    //            $(".sh-company").addClass("city-hover");
+    //            $(".sz-company,.bj-company").removeClass("city-hover");
+    //            break;
+    //        case "sz":
+    //            $(".sz-show").addClass("active");
+    //            $(".sh-show,.bj-show").removeClass("active");
+    //            $(".sh,.bj").removeClass("active");
+    //            $(".sz-company").addClass("city-hover");
+    //            $(".sh-company,.bj-company").removeClass("city-hover");
+    //            break;
+    //        case "bj":
+    //            $(".bj-show").addClass("active");
+    //            $(".sh,.sz").removeClass("active");
+    //            $(".sh-show,.sz-show").removeClass("active");
+    //            $(".bj-company").addClass("city-hover");
+    //            $(".sh-company,.sz-company").removeClass("city-hover");
+    //            break;
+    //    }
+    //};
+
+    $(".sh").on('click', function(e) {
+
+        console.log(e.target);
+
+        $(this).addClass("active").siblings().addClass("city-hover").parent().siblings().children(".city").removeClass("active").parents(".location-icon").siblings().children(".city-title").removeClass("city-hover");
+        $(this).parents(".location").siblings(".add-area").children(".sh-show").addClass("active").siblings().removeClass("active");
+        //mapClick("sh");
+    });
+
+    $(".sz").on('click', function() {
+        //$(this).addClass("active");
+        //mapClick("sz");
+        console.log($(".sz").siblings());
+
+        $(this).addClass("active").siblings().addClass("city-hover").parent().siblings().children(".city").removeClass("active").parents(".location-icon").siblings().children(".city-title").removeClass("city-hover");
+        $(this).parents(".location").siblings(".add-area").children(".sz-show").addClass("active").siblings().removeClass("active");
+    });
+
+    $(".bj").on('click', function() {
+        //$(this).addClass("active");
+        //mapClick("bj");
+        console.log($(".bj").siblings().parent().siblings().children());
+        $(this).addClass("active").siblings().addClass("city-hover").parent().siblings().children(".city").removeClass("active").parents(".location-icon").siblings().children(".city-title").removeClass("city-hover");
+        $(this).parents(".location").siblings(".add-area").children(".bj-show").addClass("active").siblings().removeClass("active");
     });
 });
