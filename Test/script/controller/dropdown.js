c2fb3c75d23ab1f34957698a2c0e9eeb053df8eb
ivy <caixinyuivy@sina.com>
2017-08-06 23:28:46 +0800

    table

--- Test/script/controller/dropdown.js
+++ Test/script/controller/dropdown.js
@@ -6,25 +6,7 @@ $(function(){
         $(this).parents("div.input-group-btn").siblings("input").val(text);
 
 
-        var rows=$(".col-lg-6:first .input-group .form-control").val(),
-            cols=$(".col-lg-6:last .input-group .form-control").val(),
-            data1=[
-                {
 
-                }
-            ],
-            container1=document.getElementById('example'),
-            hot1;
-        console.log(rows);
-        hot1=new Handsontable(container1,{
-            data:data1,
-            rowHeaders:[rows],
-            colHeaders:[cols],
-            rowWidths:100,
-            colWidths:100
-
-
-        });
     });
 
 
@@ -38,12 +20,53 @@ $(function(){
     });
 
     $("#save").off().on("click",function(){
-        var trs=$(".table>tbody>tr");
+        var values=[],
+            rows=$(".col-lg-6:first .input-group .form-control").val(),
+            cols=$(".col-lg-6:last .input-group .form-control").val();
         var inputs=$(".table>tbody>tr>td>input");
         for(var i=0;i<inputs.length;i++){
-            var items=inputs[i];
+            //var items=inputs[i];
+            values.push(inputs.eq(i).val());
+            //console.log(values[0]);
 
         }
+
+        var lists=values.sort(function(a,b){
+            return a-b;
+        });
+        console.log(lists);
+
+        var result = [];
+        for(var j=0;j<lists.length;j++){
+            if(j==0){
+                result.push(rows+"<"+lists[0])
+
+            }else{
+                result.push(lists[j-1]+"<="+rows+">"+lists[j]);
+                if(j==lists.length-1){
+                    result.push(rows+">="+lists[lists.length-1]);
+                }
+            }
+
+        }
+
+
+
+
+        var data1=[
+                result
+            ],
+            container1=document.getElementById('example'),
+            hot1;
+        hot1=new Handsontable(container1,{
+            data:data1,
+            rowHeaders:false,
+            colHeaders:false,
+            colWidths:150,
+            rowWidths:150,
+
+
+        });
     });
 
 
